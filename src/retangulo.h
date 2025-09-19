c3da0f9597a88174f6442f148e84468bbb386252
Luiz Henrique <luizhenriquealveslima857@gmail.com>
2025-09-19 11:34:02 -0300

    Update retangulo.h

---
+++ src/retangulo.h
@@ -0,0 +1,19 @@
+#ifndef RETANGULO_H
+#define RETANGULO_H
+
+/*
+
+O arquivo retangulo.h tem as definições de métodos de uma estrutura abstrata que representa um retangulo, que será utilizado em um plano bidimensional
+Sendo identificado por um id
+
+
+*/
+
+typedef void* RETANGULO;
+
+RETANGULO(int id, float altura, float largura, float x, float y, char*corb, char*corp)
+
+
+
+
+#endif
